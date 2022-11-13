d8f5d7e6c47cb83c9699e33c7f93cab1752f2096
osasmertens <r0605937@student.thomasmore.be>
2022-11-13 20:55:31 +0100

    Added constraints for changes in direction

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import game.SnakeGame;
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         SnakeGame game = new SnakeGame();
     }
 }
